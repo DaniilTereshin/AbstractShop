a8ebefbc7571bca7b86421febd95d601d2a4068d
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2018-06-08 18:38:48 +0400

    Add files via upload

---
+++ AbstractShopService/ViewModels/StoresLoadViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbstractShopService.ViewModels
+{
+    public class StoresLoadViewModel
+    {
+        public string StoreName { get; set; }
+
+        public int TotalCount { get; set; }
+
+        public IEnumerable<Tuple<string, int>> Detalis { get; set; }
+    }
+}
