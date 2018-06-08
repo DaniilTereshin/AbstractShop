fdab5a6943dac2aba2acb5789502adaf371e04c7
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2018-06-08 19:45:15 +0400

    Add files via upload

--- WpfMotorZavod/App.xaml.cs
+++ WpfMotorZavod/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿using AbstractShopService.ImplementationsList;
+﻿using AbstractShopService;
 using AbstractShopService.Interfaces;
 using System;
+using AbstractShopService.ImplementationsBD;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -29,12 +31,13 @@ namespace WpfMotorZavod
         public static IUnityContainer BuildUnityContainer()
         {
             var currentContainer = new UnityContainer();
-            currentContainer.RegisterType<IZakazchikService, ZakazchikServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IDetaliService, DetaliServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IRabochiService, RabochiServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<ICommodityService, CommodityServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IStoreService, StoreServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IMainService, MainServiceList>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<DbContext, AbstractDbContext>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IZakazchikService, ZakazchikServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IDetaliService, DetaliServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IRabochiService, RabochiServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<ICommodityService, CommodityServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IStoreService, StoreServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IMainService, MainServiceBD>(new HierarchicalLifetimeManager());
 
             return currentContainer;
         }
