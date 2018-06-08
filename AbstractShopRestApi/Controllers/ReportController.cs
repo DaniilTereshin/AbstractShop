6c94f1ba152db1d6a6de9ce9ccf8f3673527aca9
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2018-06-08 19:55:05 +0400

    Add files via upload

---
+++ AbstractShopRestApi/Controllers/ReportController.cs
@@ -0,0 +1,57 @@
+﻿using AbstractShopService.BindingModels;
+using AbstractShopService.Interfaces;
+using System;
+using System.Web.Http;
+
+namespace AbstractShopRestApi.Controllers
+{
+    public class ReportController : ApiController
+    {
+        private readonly IReportService _service;
+
+        public ReportController(IReportService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetStoresLoad()
+        {
+            var list = _service.GetStoresLoad();
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetZakazchikZakazs(ReportBindingModel model)
+        {
+            var list = _service.GetZakazchikZakazs(model);
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpPost]
+        public void SaveCommodityPrice(ReportBindingModel model)
+        {
+            _service.SaveCommodityPrice(model);
+        }
+
+        [HttpPost]
+        public void SaveStoresLoad(ReportBindingModel model)
+        {
+            _service.SaveStoresLoad(model);
+        }
+
+        [HttpPost]
+        public void SaveZakazchikZakazs(ReportBindingModel model)
+        {
+            _service.SaveZakazchikZakazs(model);
+        }
+    }
+}
