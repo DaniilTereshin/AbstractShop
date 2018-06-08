26a3a770b939d4b26cc9c00021c51746291466e8
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2018-06-08 19:42:45 +0400

    Add files via upload

--- AbstractShopModel/Rabochi.cs
+++ AbstractShopModel/Rabochi.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,9 @@ namespace AbstractShopModel
     {
         public int Id { get; set; }
 
+        [Required]
         public string RabochiFIO { get; set; }
+        [ForeignKey("RabochiId")]
+        public virtual List<Zakaz> Zakazs { get; set; }
     }
 }
