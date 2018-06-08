3346d3116bc46eb4723625a71221ba2e51170990
DaniilTereshin <32298665+DaniilTereshin@users.noreply.github.com>
2018-06-08 18:52:10 +0400

    Add files via upload

---
+++ AbstractShopRestApi/Controllers/ZakazchikController.cs
@@ -0,0 +1,57 @@
+﻿using AbstractShopService.BindingModels;
+using AbstractShopService.Interfaces;
+using System;
+using System.Web.Http;
+
+namespace AbstractShopRestApi.Controllers
+{
+    public class ZakazchikController : ApiController
+    {
+        private readonly IZakazchikService _service;
+
+        public ZakazchikController(IZakazchikService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetList()
+        {
+            var list = _service.GetList();
+            if (list == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(list);
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var element = _service.GetElement(id);
+            if (element == null)
+            {
+                InternalServerError(new Exception("Нет данных"));
+            }
+            return Ok(element);
+        }
+
+        [HttpPost]
+        public void AddElement(ZakazchikBindingModel model)
+        {
+            _service.AddElement(model);
+        }
+
+        [HttpPost]
+        public void UpdElement(ZakazchikBindingModel model)
+        {
+            _service.UpdElement(model);
+        }
+
+        [HttpPost]
+        public void DelElement(ZakazchikBindingModel model)
+        {
+            _service.DelElement(model.Id);
+        }
+    }
+}
